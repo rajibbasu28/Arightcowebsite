d60bc6e0457ec9d1e4860b722aeb158b9dd46df7
RAJIB\rajib <rajibbasu28@gmail.com>
2025-08-28 14:45:32 +0530

    rollback

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-      images: {
-    unoptimized: true, // Disable server-based image optimization
-  },
   reactStrictMode: true,
 };
 
