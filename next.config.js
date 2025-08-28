89e184dc7fcfd12d328255ea57ef524bcfde5b9e
RAJIB\rajib <rajibbasu28@gmail.com>
2025-08-28 14:35:31 +0530

    Updated package

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
   reactStrictMode: true,
 };
 
 module.exports = nextConfig;
-
-// next.config.js
-module.exports = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('aws-sdk', 'aws-amplify', '@prisma/client');
-    }
-    return config;
-  },
-};
